8e14e87e3c93be839aa6a4ce566e52608c1d6be1
Ícaro Fracassi Farias <icarofracassi@gmail.com>
2025-03-15 20:20:37 -0300

    Create dia_da_batata.py

---
+++ Code@Night/ByIrracionais/Warmup/dia_da_batata.py
@@ -0,0 +1,11 @@
+porcaoProfessores = [300, 1500, 600, 1000, 150]
+
+qtdLuciano = int(input())
+qtdMateus = int(input())
+qtdGilberto = int(input())
+qtdVinicius = int(input())
+qtdMargrit = int(input())
+qtdHelena = 1
+
+resultado = qtdLuciano*300 + qtdMateus*1500 + qtdGilberto*600 + qtdVinicius*1000 + qtdMargrit*150 + qtdHelena*225
+print (resultado)
