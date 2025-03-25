b6e7c16aaea19151ba77d3918f5def29379675ba
Ícaro Fracassi Farias <icarofracassi@gmail.com>
2025-03-25 01:22:00 -0300

    Update M-Mergulho.py

---
+++ Code@Night/ByIrracionais/M-Mergulho.py
@@ -0,0 +1,14 @@
+n2 = int(entradas[1])
+voltaram = input().split(" ")
+listaMerg = []
+resultado = ""
+if n1 == n2:
+  print("*")
+else:
+  for i in range(1, n1+1)
+    listaMerg.append(str(i))
+  for j in voltaram:
+    listaMerg.remove(j)
+  print(" ".join(listaMerg) + " ")
+
+//incompleto?
