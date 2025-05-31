26a27947877203b9356b3473c1a464bbbcea5170
Ana Beatriz Stahl <ana.beatriz.stahl@sap.com>
2025-05-31 15:48:22 -0300

    feat: change places

--- Code@Night/all-perfect-solutions/codes/P.py
+++
@@ -1,8 +0,0 @@
-# Estrutura de dados utilizada: apenas variáveis inteiras (simples)
-# Tipo de algoritmo: simulação matemática (cálculo direto)
-
-X, Y = map(int, input().split())
-# O líder ultrapassa o último quando k*X >= (k-1)*Y + 1
-# Procuramos o menor k tal que k*X > (k-1)*Y
-k = (Y - 1) // (Y - X) + 1
-print(k)
--- Code@Night/all-perfect-solutions/codes/S.py
+++
@@ -1,5 +0,0 @@
-# Estrutura de dados utilizada: apenas variáveis inteiras (simples)
-# Tipo de algoritmo: comparação direta (condicional simples)
-
-A, B = map(int, input().split())
-print(max(A, B))
